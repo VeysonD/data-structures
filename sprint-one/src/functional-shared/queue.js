14dbb5b781e450a7d1fd96dda2fb4b666ce52c91
Rick Gallegos <duulketariakan@yahoo.com>
2017-07-06 11:44:34 -0700

    Finished functional-shared queue and stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,40 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.objSize = 0;
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function (value) {
+  this.storage[this.objSize] = value;
+  //someInstance.index += 1;
+  this.objSize += 1;
+  return value;
+}
 
+queueMethods.dequeue = function () {
+  var dequeueMe;
+  var shiftingIndex = 0;
+  if (this.objSize > 0) {
+    dequeueMe = this.storage[0];
+    delete this.storage[0];
+    if (this.storage[1]) {
+      while (shiftingIndex !== this.objSize) {
+        this.storage[shiftingIndex] = this.storage[shiftingIndex + 1];
+        shiftingIndex += 1;
+      }
+      delete this.storage[shiftingIndex];
+    }
+    this.objSize -= 1;
+  }
+  return dequeueMe;
+}
+
+queueMethods.size = function () {
+  return this.objSize;
+}
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.objSize = 0;
+  someInstance.storage = {};
+
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+};
+
+stackMethods.push = function(value) {
+  this.objSize += 1;
+  this.storage[this.objSize] = value;
+};
 
 
+stackMethods.pop = function() {
+  var value = this.storage[this.objSize];
+  delete this.storage[this.objSize];
+  if (this.objSize > 0) {
+    this.objSize -= 1;
+  }
+  return value
+};
+
+stackMethods.size = function() {
+  return this.objSize;
+};
