1bf9dbdaf0b5f890a0d5bc0365654591bab7dfce
Rick Gallegos <duulketariakan@yahoo.com>
2017-07-06 01:33:53 -0700

    Finished functional queue and stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,35 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[size] = value;
+    size += 1;
+    return value;
   };
 
   someInstance.dequeue = function() {
+    var dequeueMe;
+    var shiftingIndex = 0;
+    if (size > 0) {
+      dequeueMe = storage[0];
+      delete storage[0];
+      if (storage[1]) {
+        while (shiftingIndex !== size) {
+          storage[shiftingIndex] = storage[shiftingIndex + 1];
+          shiftingIndex += 1;
+        }
+        delete storage[shiftingIndex - 1];
+      }
+      size -= 1;
+    }
+    return dequeueMe;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,42 +4,21 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
   var size = 0;
-  var count = 0;
-
 
   // Implement the methods below
   someInstance.push = function(value) {
-    count += 1;
-    //console.log(count);
     storage[size] = value;
     size++;
-    pushSize = size - 1
-    //console.log(storage[pushSize]);
-    //console.log(storage);
-    return storage[pushSize]; //can refactor this by just returning value;
+    return value;
   };
 
   someInstance.pop = function() {
-    console.log(count);
-    //console.log(size);
-    console.log(storage);
-    count += 1;
-
     if (size > 0) {
-      //console.log(size);
-      size --;
-      var popMe = storage[size];
-      //console.log(storage);
+      size--;
+      var element = storage[size];
       delete storage[size];
-      //size--;
-      //return storage[popSize];
-      return popMe;
-    } else {
-      var popMe = storage[size]
-      delete storage[size];
-      return popMe;
+      return element;
     }
-    //console.log(storage);
   };
 
   someInstance.size = function() {
