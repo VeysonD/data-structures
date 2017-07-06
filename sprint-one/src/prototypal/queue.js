9186f1a55427a8e302ffb38a91d6dde4312cfb3e
Rick Gallegos <duulketariakan@yahoo.com>
2017-07-06 15:20:27 -0700

    Completed prototypal queue and stack

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.objSize = 0;
+  someInstance.storage = {};
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.objSize] = value;
+  this.objSize += 1;
+  return value;
+};
 
+queueMethods.dequeue = function() {
+  var removeFirstItem = this.storage[0];
+  var shiftingIndex = 0;
+  delete this.storage[0];
+  if (this.objSize > 0) {
+    if (this.storage[1]) {
+      while (shiftingIndex !== this.objSize) {
+        this.storage[shiftingIndex] = this.storage[shiftingIndex + 1];
+        shiftingIndex += 1;
+      }
+      delete this.storage[shiftingIndex];
+    }
+    this.objSize -= 1;
+  }
+  return removeFirstItem;
+};
+
+queueMethods.size = function() {
+  return this.objSize;
+}
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.objSize = 0;
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.objSize] = value;
+  this.objSize += 1;
 
+  return value;
+};
+
+stackMethods.pop = function() {
+  var lastValue;
+  if (this.objSize > 0) {
+    this.objSize -= 1;
+    lastValue = this.storage[this.objSize];
+    delete this.storage[this.objSize];
+  }
+  return lastValue;
+};
+
+stackMethods.size = function() {
+  return this.objSize;
+};
