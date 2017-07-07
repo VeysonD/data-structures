4aba9c12c315bc34355714b17378fba24986539a
Rick Gallegos <duulketariakan@yahoo.com>
2017-07-06 19:21:37 -0700

    Completed Data Structures Sprint

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,34 @@
 var Queue = function() {
+  this.objSize = 0;
+  this.storage = {};
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.objSize] = value;
+  this.objSize += 1;
+  return value;
+};
 
+
+Queue.prototype.dequeue = function() {
+  var removeFirst = this.storage[0];
+  var shiftingIndex = 0;
+  delete this.storage[0];
+  if (this.objSize > 0) {
+    if (this.storage[1]) {
+      while (shiftingIndex !== this.objSize) {
+        this.storage[shiftingIndex] = this.storage[shiftingIndex + 1];
+        shiftingIndex += 1;
+      }
+      delete this.storage[shiftingIndex];
+    }
+    this.objSize -= 1;
+  }
+  return removeFirst;
+};
+
+Queue.prototype.size = function () {
+  return this.objSize;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,27 @@
 var Stack = function() {
+  this.objSize = 0;
+  this.storage = {};
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
 
+Stack.prototype.push = function(value) {
+  this.storage[this.objSize] = value;
+  this.objSize += 1;
+  return value;
+};
+
+Stack.prototype.pop = function() {
+  var removeLast;
+  if (this.objSize > 0) {
+    this.objSize -= 1;
+    removeLast = this.storage[this.objSize];
+    delete this.storage[this.objSize];
+  }
+  return removeLast;
+};
+
+Stack.prototype.size = function() {
+  return this.objSize;
+};
