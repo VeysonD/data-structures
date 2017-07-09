b6f46312d968406fa2bb13dd42426b527a1811f8
Rick Gallegos <duulketariakan@yahoo.com>
2017-07-09 00:43:19 -0700

    Working hash table with collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,15 +6,32 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+  //console.log(this);
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var holder = this._storage.get(index)
+
+  if (holder === undefined) {
+    var bucket = [k, v]
+    this._storage.set(index, bucket);
+  } else {
+    holder[1] = v;
+  }
+  console.log('done');
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var holder = this._storage.get(index);
+  if (holder) {
+    return holder[1];
+  } else {
+    return undefined;
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
@@ -22,5 +39,3 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
